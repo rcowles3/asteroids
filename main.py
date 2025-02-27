321727b11aff5b120cece8fcfb60ecf1510b9d96
rcowles3 <ryan.cowles3@gmail.com>
2025-02-26 22:39:26 -0700

    updated fps

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ def main():
   print(f"Screen height: {SCREEN_HEIGHT}")
   pygame.init()
 
-  # Create Screen
+  game_clock = pygame.time.Clock()  
+  dt = 0
+
+  # Set Screen Variables
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   
+  # Create Screen
   while True:
     
     # allows user to click to close program
@@ -23,7 +27,9 @@ def main():
           return
     
     screen.fill('#000000')
-    pygame.display.flip()     
+    pygame.display.flip()   
+
+    dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
