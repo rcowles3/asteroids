0b35bf0678bc30ca157bfda54c64ef67c8d4a0ff
rcowles3 <ryan.cowles3@gmail.com>
2025-03-03 22:21:24 -0700

    player drawn

--- main.py
+++ main.py
@@ -2,34 +2,45 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
 from constants import *
+from player import *
 
 def main():
 
-  # Init Game
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-  pygame.init()
+	# Init Game
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
 
-  game_clock = pygame.time.Clock()  
-  dt = 0
+	game_clock = pygame.time.Clock()  
+	dt = 0
 
-  # Set Screen Variables
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	# Set Screen Variables
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	# Init Player
+	player_x = SCREEN_WIDTH / 2
+	player_y = SCREEN_HEIGHT / 2
+	player = Player(player_x, player_y)
   
-  # Create Screen
-  while True:
+	# Create Screen
+	while True:
     
-    # allows user to click to close program
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          return
-    
-    screen.fill('#000000')
-    pygame.display.flip()   
+		# allows user to click to close program
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
-    dt = game_clock.tick(60) / 1000
+		screen.fill('#000000')
+
+		# draw player
+		player.draw(screen)
+		
+		pygame.display.flip()   
+
+		dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
